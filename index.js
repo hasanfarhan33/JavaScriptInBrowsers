f78a1dfa3e4d7efa871fe2f8af540788b3615e83
Farhan Hasan <hasanfarhan33@gmail.com>
2021-05-03 13:46:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// This is a comment in Java Script 
+console.log("Hello World");  //This is used to print in the console of JS 
+let firstName = 'Farhan';
+let lastName = "Hasan"; 
+console.log(firstName);
+console.log(lastName); 
+
+const interstRate = 0.3; //Constant variable in JavaScript 
+let isApproved = false; //Boolean Literal 
+let Name = undefined;
+let selectedColor = null; 
+let number = 1; //Number Literal  
